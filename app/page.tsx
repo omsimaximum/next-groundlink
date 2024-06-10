089299a70ff9736696ed0099ea5685bb6a99d8e2
joshua <joshuagementiza@gmail.com>
2024-06-10 08:22:16 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+import { NAVLINKS } from "@/constants/store";
 import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <div>
-      <div className="h-[15vh] bg-red-200">
+    <div className="bg-red-500 px-[150px] mx-auto" >
+      <div className=" bg-red-200">
         <h1>Groundlink</h1>
-        <p>asdasd</p>
+        <ul>
+          {NAVLINKS.map(item => (
+            <li key={item.name}>
+              <a href={item.url}>{item.name}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
---
+++ constants/store.ts
@@ -0,0 +1,28 @@
+interface INavLinks {
+  url: string,
+  name: string
+}
+
+
+export const NAVLINKS : INavLinks[] = [
+  {
+    url:'#',
+    name:'Home'
+  },
+  {
+    url:'#',
+    name:'About us'
+  },
+  {
+    url:'#',
+    name:'Services'
+  },
+  {
+    url:'#',
+    name:'Pricing'
+  },
+  {
+    url:'#',
+    name:'Contact'
+  },
+]
